bd59ad3f394118ebfe37d952412bfb4f9dd51ee7
Gustavo Bertti <88666639+Gustavo-Bertti@users.noreply.github.com>
2024-02-14 12:46:49 -0300

    Add files via upload

---
+++ ExerciciosDeOrientaçãoObjetos/ExerciciosDeOrientaçãoObjetos/ContaBancaria.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosDeOrientaçãoObjetos
+{
+    internal class ContaBancaria
+    {
+        ///atributos e auto properties
+        private int _numeroConta { get; set; }
+        private string _titular { get; set; } = string.Empty;
+
+        private double _saldo { get; set; }
+  
+
+        //Construtor
+        public ContaBancaria(int numeroConta, string titular, double saldo) {
+            _numeroConta = numeroConta;
+            _titular = titular;
+            _saldo = saldo;
+        }
+
+        //metodos
+        public override string ToString()
+        {
+            return $"Conta: {_numeroConta}, Titular: {_titular}, Saldo: {_saldo}";
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0) {
+                _saldo += valor;
+                Console.WriteLine("Dados da conta atulizado");
+                Console.WriteLine(ToString());
+
+            }
+        
+        }
+
+        public void Saque(double valor)
+        {
+            if (valor > 0) {
+                if (_saldo > valor)
+                {
+                    _saldo -= valor;
+                    Console.WriteLine("Dados da conta atulizado");
+                    Console.WriteLine(ToString());
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Digite um valor maior que 0");
+            }
+           
+         
+        }
+
+
+
+
+
+
+    }
+}
---
+++ ExerciciosDeOrientaçãoObjetos/ExerciciosDeOrientaçãoObjetos/Program.cs
@@ -0,0 +1,10 @@
+﻿using ExerciciosDeOrientaçãoObjetos;
+
+ContaBancaria novaConta = new ContaBancaria(8823, "Gustavo", 600);
+
+Console.WriteLine(novaConta);
+
+novaConta.Depositar(200);
+
+novaConta.Saque(400);
+
